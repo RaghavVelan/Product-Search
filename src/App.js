f2696fe04ff6a1b31419b22ac6b53b0f6300f611
RaghavVelan <vraghav950@gmail.com>
2024-01-05 10:43:51 +0530

    added env

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import {useState} from 'react';
+import fetchApi from './service/fetchApi';
 import shoppingCart from "./assets/shopping-cart.png";
 import searchIcon from "./assets/search.svg"
-import axios from 'axios';
 import './App.css';
 import ProductListing from './components/Product-Listing';
 
 
-const baseUrl = 'https://real-time-product-search.p.rapidapi.com/search';
+
 
 function App() {
 
+  let title = "Product Search";
+
   const [searchInput, setSearchInput] = useState('');
-  const [updatedInput, setUpdatedInput] = useState('');
+  // const [updatedInput, setUpdatedInput] = useState('');
   const [productDetails, setProductDetails] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchProduct = () => {
     setIsLoading(true);
-    axios.get(baseUrl, {
-      params: {
-      q: searchInput,
-      country: 'in',
-      language: 'en',
-      limit: '10'
-      },
-      headers: {
-      'X-RapidAPI-Key': '796680dbedmsha46932a79c26027p133d88jsn9c70d71bcd24',
-      'X-RapidAPI-Host': 'real-time-product-search.p.rapidapi.com'
-      }
-    })
-    .then((response) => {
-      setProductDetails(response.data.data);
-    })
+    fetchApi(searchInput, setProductDetails)
     setIsLoading(false);
   }
 
@@ -48,7 +36,7 @@ function App() {
   return (
     <div className="App">
         <div className='header'>
-          <h1>OFFER TRACKER</h1>
+          <h1>{title}</h1>
         </div>
         {/* Search bar */}
         <div className="search-container">
@@ -61,7 +49,7 @@ function App() {
         productDetails.length === 0 ? 
         <div className='HomePage'>
          {
-           isLoading == false ? <div className="loader"> <img src="https://img.icons8.com/bubbles/100/000000/loading-sign.png" alt="loading-sign"/> <p>Loading</p></div>
+           isLoading === false ? <div className="loader"> <img src="https://img.icons8.com/bubbles/100/000000/loading-sign.png" alt="loading-sign"/> <p>Loading</p></div>
           :
           <div className='container-animation'>
             <img src={shoppingCart} alt="add-shopping-cart"/>
